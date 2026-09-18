c3c3ca29d551c4263169a87764e030ac8af44004
himanshu25100 <himanshu.25bai10080@vitbhopal.ac.in>
2026-09-18 22:48:06 +0530

    Delete clinic-care directory

--- clinic-care/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Compiles the application and the tests into out/
-set -e
-rm -rf out
-mkdir -p out
-javac -d out $(find src test -name "*.java")
-echo "Build OK -> out/"
-echo "Run app  : java -cp out com.clinic.Main"
-echo "Run tests: java -cp out com.clinic.test.SelfTest"
